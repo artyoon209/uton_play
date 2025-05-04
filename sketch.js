3c77062b3c8f244005fbc09ae55dc821969872a9
artyoon209 <158546759+artyoon209@users.noreply.github.com>
2025-05-04 12:15:15 +0900

    B18: sketch.js 점 생성 복원 및 사운드 개선

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 let dots = [];
 
 function mousePressed() {
@@ -14,104 +13,44 @@ function touchStarted() {
     playNote(mouseY, mouseX);
   }
 }
-}
-
-function playNote(yPos, xPos) {
-  let pan = map(xPos, 0, width, -1, 1);
-  let step = height / notes.length;
-  let freqIndex = constrain(floor(yPos / step), 0, notes.length - 1);
-  let freq = notes[notes.length - 1 - freqIndex];
-
-  let osc = new p5.Oscillator('sine');
-  osc.freq(freq);
-  osc.pan(pan);
-
-  let env = new p5.Envelope();
-  env.setADSR(0.05, 0.3, 0.0, 0.6);  // attack, decay, sustain, release
-  env.setRange(0.05, 0);             // max amp, min amp
-
-  osc.start();
-  env.play(osc, 0, 0.05);
-}
 
 class Dot {
   constructor(x, y) {
-    this.pos = createVector(x, y);
-    this.baseRadius = 5;
-    this.radius = this.baseRadius;
-    this.maxRadius = random(30, 80);
-    this.growthSpeed = 0.5;
-    this.locked = false;
-    this.color = color(`hsb(${selectedHue}, ${saturationSlider.value()}%, ${brightnessSlider.value()}%)`);
-    this.shapePoints = [];
+    this.x = x;
+    this.y = y;
+    this.r = 5;
+    this.maxR = 28;
+    this.c = color(255);
   }
 
-  update(others) {
-    if (this.locked) return;
-    let canGrow = true;
-    for (let other of others) {
-      if (other === this) continue;
-      let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
-      if (d < this.radius + other.radius - 6) {
-        canGrow = false;
-        break;
-      }
-    }
-    if (canGrow && this.radius < this.maxRadius) {
-      this.radius += this.growthSpeed;
-    } else {
-      this.locked = true;
-      this.captureShape(others);
-    }
-  }
-
-  captureShape(others) {
-    this.shapePoints = [];
-    for (let i = 0; i <= resolution; i++) {
-      let angle = TWO_PI * i / resolution;
-      let x = cos(angle);
-      let y = sin(angle);
-      let r = this.radius;
-      for (let other of dots) {
-        if (other === this) continue;
-        let testPoint = p5.Vector.add(this.pos, createVector(x, y).mult(this.radius));
-        let d = dist(testPoint.x, testPoint.y, other.pos.x, other.pos.y);
-        if (d < this.radius + other.radius - 2) {
-          r -= map(this.radius + other.radius - d, 0, this.radius, 0, 8);
-        }
-      }
-      let vx = this.pos.x + x * r;
-      let vy = this.pos.y + y * r;
-      this.shapePoints.push(createVector(vx, vy));
+  update() {
+    if (this.r < this.maxR) {
+      this.r += 0.25;
     }
   }
 
   display() {
-    stroke(this.color);
-    beginShape();
-    if (this.locked && this.shapePoints.length > 0) {
-      for (let pt of this.shapePoints) {
-        curveVertex(pt.x, pt.y);
-      }
-    } else {
-      for (let i = 0; i <= resolution; i++) {
-        let angle = TWO_PI * i / resolution;
-        let x = cos(angle);
-        let y = sin(angle);
-        let r = this.radius;
-        for (let other of dots) {
-          if (other === this) continue;
-          let testPoint = p5.Vector.add(this.pos, createVector(x, y).mult(this.radius));
-          let d = dist(testPoint.x, testPoint.y, other.pos.x, other.pos.y);
-          if (d < this.radius + other.radius - 2) {
-            r -= map(this.radius + other.radius - d, 0, this.radius, 0, 8);
-          }
-        }
-        let vx = this.pos.x + x * r;
-        let vy = this.pos.y + y * r;
-        curveVertex(vx, vy);
-      }
+    stroke(this.c);
+    noFill();
+    ellipse(this.x, this.y, this.r);
+  }
+
+  react(other) {
+    let d = dist(this.x, this.y, other.x, other.y);
+    if (d < 100) {
+      stroke(100, 100);
+      line(this.x, this.y, other.x, other.y);
     }
-    endShape(CLOSE);
+  }
+}
+
+function draw() {
+  background(0, 20);
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].update();
+    dots[i].display();
+    for (let j = i + 1; j < dots.length; j++) {
+      dots[i].react(dots[j]);
+    }
   }
 }
